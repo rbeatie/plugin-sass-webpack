3aed19ae72f5f71a0dc37da83202af476b03dc85
rbeatie <rexbeartie@MagnaCarta.local>
2019-08-29 12:46:50 -0700

    initial commit with webpack.config.js and the SASS loaders

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as FlexPlugin from 'flex-plugin';
+import TestCracoPlugin from './TestCracoPlugin';
+import * as Alt from './alt-index';
+
+import './main.scss';
+
+
+FlexPlugin.loadPlugin(TestCracoPlugin);
+
